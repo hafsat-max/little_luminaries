f750779cecae6671bd44c2d97d72acb2acb4549a
Abiodun Busari <hafsahbusari69@gmail.com>
2025-05-21 22:36:41 +0200

    feat: setting up

---
+++ components/header-strip.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const HeaderStrip = () => {
+  return (
+    <div className="py-5 bg-blue-primary flex items-center justify-between">
+      <section className="flex items-center gap-2"></section>
+    </div>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        blue: {
+          primary: "#15254E",
+        },
       },
     },
   },
