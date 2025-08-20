c62b2d54290448a844f11c7ba53a32583a605979
Abiodun Busari <hafsahbusari69@gmail.com>
2025-08-20 13:20:13 +0200

    feat: added analytics tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${mcLaren.className} ${manrope.className} antialiased`}>
+      <body className={`${mcLaren.className} antialiased`}>
         <MantineProvider>{children}</MantineProvider>
         <Analytics />
       </body>
--- components/sections/cta-section.tsx
+++ components/sections/cta-section.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { manrope, mcLaren } from "@/app/layout";
+// import { manrope, mcLaren } from "@/app/layout";
 import React from "react";
 import { LumiRed } from "../icons/lumi-red";
 import AppButton from "../common/app-button";
@@ -9,7 +9,7 @@ import { track } from "@vercel/analytics";
 export const CTASection = () => {
   return (
     <section
-      className={` py-12 lg:py-24  xl:px-28 lg:px-20 sm:px-12 px-5  flex sm:flex-row flex-col lg:h-[833px] sm:h-[600px] h-full bg-[#E9F4F7] items-center justify-between ${manrope.className} lg:gap-0 gap-5`}
+      className={` py-12 lg:py-24  xl:px-28 lg:px-20 sm:px-12 px-5  flex sm:flex-row flex-col lg:h-[833px] sm:h-[600px] h-full bg-[#E9F4F7] items-center justify-between lg:gap-0 gap-5`}
     >
       <img
         src="/cta.svg"
@@ -22,20 +22,22 @@ export const CTASection = () => {
           className={`flex gap-2 items-center border border-blue-secondary  rounded-full px-3.5 py-7px mb-2  w-fit`}
         >
           <LumiRed color="#3AC8EE" />
-          <span className="text-sm font-semibold text-black">LET’S TALK</span>
+          <span className="text-sm font-semibold text-black">
+            LET&apos;S TALK
+          </span>
         </div>
         <p
           className={`xl:text-6xl text-4xl font-semibold text-black mb-4 leading-tight `}
         >
-          Every Child is Unique – Let’s Find the Right Path
+          Every Child is Unique – Let&apos;s Find the Right Path
         </p>
         <p className=" mb-8  text-base text-[#4B5563]">
           Want to learn more about how Little Luminaries can help your child
           succeed? Schedule a free consultation with our tutors to discuss your
-          child’s learning needs and explore the best program for them.
+          child&apos;s learning needs and explore the best program for them.
         </p>
         <AppButton
-          className={`w-fit ${mcLaren.className} lg:py-[18px] lg:px-8 py-[14px] px-6`}
+          className={`w-fit lg:py-[18px] lg:px-8 py-[14px] px-6`}
           icon={<Calendar />}
           text="Book a free consultation"
           onClick={() => track("buy_clicked", { plan: "hobby" })}
